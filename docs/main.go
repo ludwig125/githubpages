f0692878a9e29c431c129cd6196c7039454a03b3
ludwig125 <18366858+ludwig125@users.noreply.github.com>
2021-12-30 07:41:46 +0900

    add sub

--- docs/main.go
+++ docs/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"syscall/js"
+)
 
 func main() {
+	c := make(chan struct{})
+
 	fmt.Println("Hello, WebAssembly!")
+	registerCallbacks()
+	<-c
+}
+
+func add(this js.Value, args []js.Value) interface{} {
+	js.Global().Set("output", js.ValueOf(args[0].Int()+args[1].Int()))
+	println(js.ValueOf(args[0].Int() + args[1].Int()).String())
+	return nil
+}
+
+func subtract(this js.Value, args []js.Value) interface{} {
+	js.Global().Set("output", js.ValueOf(args[0].Int()-args[1].Int()))
+	println(js.ValueOf(args[0].Int() - args[1].Int()).String())
+	return nil
+}
+
+func registerCallbacks() {
+	js.Global().Set("add", js.FuncOf(add))
+	js.Global().Set("subtract", js.FuncOf(subtract))
 }
