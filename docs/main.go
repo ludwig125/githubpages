b1efa0c7695867fa6d881b72c30731c6680c3d3e
ludwig125 <18366858+ludwig125@users.noreply.github.com>
2021-12-31 06:54:24 +0900

    print ans

--- docs/main.go
+++ docs/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"syscall/js"
 )
 
@@ -14,15 +15,37 @@ func main() {
 }
 
 func add(this js.Value, args []js.Value) interface{} {
-	js.Global().Set("output", js.ValueOf(args[0].Int()+args[1].Int()))
-	println(js.ValueOf(args[0].Int() + args[1].Int()).String())
+	value1 := js.Global().Get("document").Call("getElementById", args[0].String()).Get("value").String()
+	value2 := js.Global().Get("document").Call("getElementById", args[1].String()).Get("value").String()
+
+	int1, _ := strconv.Atoi(value1)
+	int2, _ := strconv.Atoi(value2)
+
+	fmt.Println("int1:", int1, " int2:", int2)
+
+	js.Global().Set("output", int1+int2)
+	println(int1 + int2)
+	answer(int1 + int2)
 	return nil
 }
 
 func subtract(this js.Value, args []js.Value) interface{} {
-	js.Global().Set("output", js.ValueOf(args[0].Int()-args[1].Int()))
-	println(js.ValueOf(args[0].Int() - args[1].Int()).String())
+	value1 := js.Global().Get("document").Call("getElementById", args[0].String()).Get("value").String()
+	value2 := js.Global().Get("document").Call("getElementById", args[1].String()).Get("value").String()
+
+	int1, _ := strconv.Atoi(value1)
+	int2, _ := strconv.Atoi(value2)
+
+	fmt.Println("int1:", int1, " int2:", int2)
+
+	js.Global().Set("output", int1+int2)
+	println(int1 - int2)
+	answer(int1 - int2)
 	return nil
+}
+
+func answer(ans int) {
+	js.Global().Get("document").Call("getElementById", "answer").Set("innerHTML", ans)
 }
 
 func registerCallbacks() {
