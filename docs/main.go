edd920752b6f8290a2ac9cb5dfc9660e2a352d77
ludwig125 <18366858+ludwig125@users.noreply.github.com>
2022-01-20 06:40:35 +0900

    error handling calclator

--- docs/main.go
+++ docs/main.go
@@ -1,55 +1,97 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
 	"syscall/js"
 )
 
 func main() {
-	c := make(chan struct{})
-
-	fmt.Println("Hello, WebAssembly!")
 	registerCallbacks()
-	<-c
+	<-make(chan struct{})
 }
 
 func registerCallbacks() {
-	js.Global().Set("add", js.FuncOf(add))
-	js.Global().Set("subtract", js.FuncOf(subtract))
+	js.Global().Set("calcAdd", calculatorWrapper("add"))
+	js.Global().Set("calcSubtract", calculatorWrapper("subtract"))
 }
 
-func add(this js.Value, args []js.Value) interface{} {
-	value1 := textToStr(args[0])
-	value2 := textToStr(args[1])
+func calculatorWrapper(ope string) js.Func {
+	calcFunc := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		value1, err := getJSValue(args[0].String())
+		if err != nil {
+			return wrapResult("", err)
+		}
+		value2, err := getJSValue(args[1].String())
+		if err != nil {
+			return wrapResult("", err)
+		}
+		fmt.Println("value1:", value1, " value2:", value2)
 
-	int1, _ := strconv.Atoi(value1)
-	int2, _ := strconv.Atoi(value2)
-	fmt.Println("int1:", int1, " int2:", int2)
-	ans := int1 + int2
+		int1, err := strconv.Atoi(value1)
+		if err != nil {
+			return wrapResult("", fmt.Errorf("failed to convert value1 to int: %v", err))
+		}
+		int2, err := strconv.Atoi(value2)
+		if err != nil {
+			return wrapResult("", fmt.Errorf("failed to convert value2 to int: %v", err))
+		}
 
-	printAnswer(ans)
+		var ans int
+		switch ope {
+		case "add":
+			ans = int1 + int2
+		case "subtract":
+			ans = int1 - int2
+		default:
+			return wrapResult("", fmt.Errorf("invalid operation: %s", ope))
+		}
+		fmt.Println("Answer:", ans)
+
+		if err := setJSValue("answer", ans); err != nil {
+			return wrapResult("", err)
+		}
+		return nil
+	})
+	return calcFunc
+}
+
+func getJSValue(elemID string) (string, error) {
+	jsDoc := js.Global().Get("document")
+	if !jsDoc.Truthy() {
+		return "", errors.New("failed to get document object")
+	}
+
+	jsElement := jsDoc.Call("getElementById", elemID)
+	if !jsElement.Truthy() {
+		return "", fmt.Errorf("failed to getElementById: %s", elemID)
+	}
+
+	jsValue := jsElement.Get("value")
+	if !jsValue.Truthy() {
+		return "", fmt.Errorf("failed to Get value: %s", elemID)
+	}
+	return jsValue.String(), nil
+}
+
+func setJSValue(elemID string, value interface{}) error {
+	jsDoc := js.Global().Get("document")
+	if !jsDoc.Truthy() {
+		return errors.New("failed to get document object")
+	}
+
+	jsElement := jsDoc.Call("getElementById", elemID)
+	if !jsElement.Truthy() {
+		return fmt.Errorf("failed to getElementById: %s", elemID)
+	}
+	jsElement.Set("innerHTML", value)
 	return nil
 }
 
-func subtract(this js.Value, args []js.Value) interface{} {
-	value1 := textToStr(args[0])
-	value2 := textToStr(args[1])
-
-	int1, _ := strconv.Atoi(value1)
-	int2, _ := strconv.Atoi(value2)
-	fmt.Println("int1:", int1, " int2:", int2)
-	ans := int1 - int2
-
-	printAnswer(ans)
-	return nil
-}
-
-func textToStr(v js.Value) string {
-	return js.Global().Get("document").Call("getElementById", v.String()).Get("value").String()
-}
-
-func printAnswer(ans int) {
-	println(ans)
-	js.Global().Get("document").Call("getElementById", "answer").Set("innerHTML", ans)
+func wrapResult(result string, err error) map[string]interface{} {
+	return map[string]interface{}{
+		"error":    err.Error(),
+		"response": result,
+	}
 }
