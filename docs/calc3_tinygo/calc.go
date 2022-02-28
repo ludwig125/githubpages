dd80acf4b3d3d67e121a68df98e210679dbaa122
ludwig125 <18366858+ludwig125@users.noreply.github.com>
2022-03-01 06:32:37 +0900

    fix

--- docs/calc3_tinygo/calc.go
+++ docs/calc3_tinygo/calc.go
@@ -13,68 +13,12 @@ func main() {
 }
 
 func registerCallbacks() {
-	fmt.Println("register")
-	js.Global().Set("calcAdd2", calculatorWrapper("add"))
-	// js.Global().Set("calcSubtract", calculatorWrapper("subtract"))
+	js.Global().Set("calcAdd", calculatorWrapper("add"))
+	js.Global().Set("calcSubtract", calculatorWrapper("subtract"))
 }
-
-// //export calcAdd
-// func calcAdd(x, y string) {
-// 	fmt.Println("hoge")
-// }
-
-// //export calcAdd
-// func calcAdd() map[string]interface{} {
-// 	println("calcAdd")
-// 	// fmt.Printf("calcAdd x,y: '%s %s'\n", x, y)
-// 	return calculatorWrapper("add")
-// }
-
-// //export calcAdd2
-// func calcAdd2(x string) map[string]interface{} {
-// 	println("yahooooo2")
-// 	fmt.Printf("calcAdd2 x,y: '%s'\n", x)
-// 	// return calculatorWrapper("add", x, x)
-// 	return nil
-// }
-
-// //export calcAdd3
-// func calcAdd3(s string) map[string]interface{} {
-// 	fmt.Printf("yahooooo2: '%s'\n", s)
-// 	x, y, err := decodeParams(s)
-// 	if err != nil {
-// 		return wrapResult("", fmt.Errorf("failed to decodeParams: %v", err))
-// 	}
-// 	fmt.Printf("calcAdd3 x,y: '%s %s'\n", x, y)
-// 	// return calculatorWrapper("add", x, x)
-// 	return nil
-// }
-
-// //export add
-// func add(x int, y int) int {
-// 	fmt.Println("add x,y",x, y)
-// 	return x + y
-//   }
-
-// //export calcSubtract
-// func calcSubtract() map[string]interface{} {
-// 	return calculatorWrapper("subtract")
-// }
 
 func calculatorWrapper(ope string) js.Func {
 	calcFunc := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		fmt.Println("calc")
-		// value1, err := getJSValue(args[0].String())
-		// if err != nil {
-		// 	return wrapResult("", err)
-		// }
-		// value2, err := getJSValue(args[1].String())
-		// if err != nil {
-		// 	return wrapResult("", err)
-		// }
-		// fmt.Println("value1:", value1, " value2:", value2)
-
-		// func calculatorWrapper(ope string) map[string]interface{} {
 		value1, err := getJSValue("value1")
 		if err != nil {
 			return wrapResult("", err)
